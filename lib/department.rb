bdf19db3ec87e6b6b5f43f6536d189ef64b63efa
pallavi0810 <rupai_810@yahoo.co.in>
2015-06-10 15:38:30 +0530

    added class definition for department

--- lib/department.rb
+++ lib/department.rb
@@ -1,3 +1,12 @@
 # represents the financial situation of a department
 class Department
+
+  def initialize(department_balance)
+    @department_balance = department_balance
+  end
+
+  def get_remaining_cash
+    @department_balance
+  end
+   
 end
